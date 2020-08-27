ab4b63658b70c6c391693cb19d94412ff672b5eb
David Wright <dontbothertheking@gmail.com>
2020-08-27 17:24:14 -0500

    Add UserAccountController

---
+++ Bulliten.API/Controllers/UserAccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bulliten.API.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserAccountController : ControllerBase
+    {
+        private readonly ILogger<UserAccountController> _logger;
+
+        public UserAccountController(ILogger<UserAccountController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
