870840d11ecdc10193f55993165bdc3931a6b9ff
David Wright <dontbothertheking@gmail.com>
2020-08-27 16:33:15 -0500

    Add register form shell

---
+++ bulliten.vue/src/components/RegisterForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form>
+    <v-text-field
+      v-model="username"
+      label="Username"
+      required
+    />
+
+    <v-text-field
+      v-model="password"
+      type="password"
+      label="Password"
+      required
+    />
+
+    <v-btn @click="submit">
+      Submit
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    username: "",
+    password: ""
+  }),
+  methods: {
+    submit() {
+
+    }
+  }
+});
+</script>
--- bulliten.vue/src/views/Register.vue
+++ bulliten.vue/src/views/Register.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-container fill-height>
-
+  <v-container
+    class="d-flex justify-center"
+    fill-height
+  >
+    <RegisterForm/>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import RegisterForm from "@/components/RegisterForm.vue";
 
 export default Vue.extend({
+  components: {
+    RegisterForm
+  },
   data: () => ({
 
   })
