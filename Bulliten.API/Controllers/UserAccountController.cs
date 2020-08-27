a509da0fe06e67c7e942b9398560a11b2f153e47
David Wright <dontbothertheking@gmail.com>
2020-08-27 17:34:32 -0500

    Add basic CreateAccount and GetUsers routes

--- Bulliten.API/Controllers/UserAccountController.cs
+++ Bulliten.API/Controllers/UserAccountController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
+using Bulliten.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -13,9 +15,32 @@ namespace Bulliten.API.Controllers
     {
         private readonly ILogger<UserAccountController> _logger;
 
+        private static List<UserAccount> _accounts = new List<UserAccount>
+        {
+            new UserAccount { Username = "test", Password = "test" }
+        };
+
         public UserAccountController(ILogger<UserAccountController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpGet("all")]
+        public IEnumerable<UserAccount> GetUsers()
+        {
+            return _accounts;
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateAccount([FromForm]UserAccount formAccount)
+        {
+            var isInvalidUsername = _accounts.Any(u => u.Username == formAccount.Username);
+
+            if (isInvalidUsername) 
+                return Problem("Username already in use", statusCode: 400);
+
+            _accounts.Add(formAccount);
+            return StatusCode(201);
         }
     }
 }
