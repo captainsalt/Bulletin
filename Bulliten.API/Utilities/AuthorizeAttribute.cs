1d4eb22f079eff7bb64287719e10f2ed66d9de23
David Wright <dontbothertheking@gmail.com>
2020-08-27 17:45:37 -0500

    Add AuthorizeAttribute

---
+++ Bulliten.API/Utilities/AuthorizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Bulliten.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Utilities
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (UserAccount)context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                context.Result = new JsonResult("Unauthorized")
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}
