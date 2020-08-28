549d12377264b88b4eddab1671a55cdc27ddf50f
David Wright <dontbothertheking@gmail.com>
2020-08-27 20:00:40 -0500

    Add UserAccountService

---
+++ Bulliten.API/Services/IUserAccountService.cs
@@ -0,0 +1,13 @@
+﻿using Bulliten.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Services
+{
+    public interface IUserAccountService
+    {
+        Task<AuthenticationResponse> Authenticate(AuthenticationRequest model);
+    }
+}
---
+++ Bulliten.API/Services/UserAccountService.cs
@@ -0,0 +1,57 @@
+﻿using Bulliten.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Services
+{
+    public class UserAccountService : IUserAccountService
+    {
+        private readonly IConfiguration _config;
+        private readonly BullitenDBContext _context;
+
+        public UserAccountService(IConfiguration config, BullitenDBContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        public async Task<AuthenticationResponse> Authenticate(AuthenticationRequest model)
+        {
+            var user = await _context.UserAccounts.SingleOrDefaultAsync(x => x.Username == model.Username && x.Password == model.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = GenerateJwtToken(user);
+
+            return new AuthenticationResponse(user, token);
+        }
+
+        private string GenerateJwtToken(UserAccount user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Secret"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("ID", user.ID.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
