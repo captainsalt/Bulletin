6799be50c09af6e2a75bd4a0b77e7fd1ebb7828c
David Wright <dontbothertheking@gmail.com>
2020-08-28 02:18:46 -0500

    Remove unnecessary assignment

--- bulliten.vue/src/services/api-interface.ts
+++ bulliten.vue/src/services/api-interface.ts
@@ -2,16 +2,14 @@ import axios from "axios";
 
 const baseUrl: string = process.env.VUE_APP_API_URL;
 
-export async function createAccount(username: string, password: string): Promise<Response> {
+export function createAccount(username: string, password: string): Promise<Response> {
   const form = new FormData();
   form.append("username", username);
   form.append("password", password);
 
-  const response = await fetch(`${baseUrl}/api/user/create`, {
+  return fetch(`${baseUrl}/api/user/create`, {
     method: "POST",
     mode: "cors",
     body: form
   });
-
-  return response;
 }
