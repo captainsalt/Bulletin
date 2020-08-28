e42e5e02b1c871e8094f82b74b20573cc1c6402d
David Wright <dontbothertheking@gmail.com>
2020-08-28 01:49:15 -0500

    Add Error class

---
+++ Bulliten.API/Models/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Models
+{
+    public class Error
+    {
+        public string Message { get; set; }
+
+        public int Code { get; set; }
+
+        public Error(string message, int code)
+        {
+            Message = message;
+            Code = code;
+        }
+    }
+}
