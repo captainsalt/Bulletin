42d6b89751b1272b3c38bc8e45896aca4e86378b
David Wright <dontbothertheking@gmail.com>
2020-09-01 23:33:11 -0500

    Remove required attributes from AuthenticationRequest

--- Bulliten.API/Models/AuthenticationRequest.cs
+++ Bulliten.API/Models/AuthenticationRequest.cs
@@ -8,10 +8,8 @@ namespace Bulliten.API.Models
 {
     public class AuthenticationRequest
     {
-        [Required]
         public string Username { get; set; }
 
-        [Required]
         public string Password { get; set; }
     }
 }
