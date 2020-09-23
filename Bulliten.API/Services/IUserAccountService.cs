809a0ceb49d65f905950bdddbab2a38bb30b0caf
David Wright <dontbothertheking@gmail.com>
2020-09-23 15:20:37 -0500

    Add IUserAccountService

---
+++ Bulliten.API/Services/IUserAccountService.cs
@@ -0,0 +1,21 @@
+﻿using Bulliten.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Services
+{
+    interface IUserAccountService
+    {
+        UserAccount GetUserByUsername(string username);
+
+        Task FollowUser(string username);
+
+        Task UnfollowUser(string username);
+
+        Task CreateAccount(string username, string password);
+
+        Task Login(string username, string password);
+    }
+}
