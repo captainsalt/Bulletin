e811312fdf445a6261b772ec1fe07da2cdfd9c8c
David Wright <dontbothertheking@gmail.com>
2020-09-22 22:01:39 -0500

    Run code cleanup

--- Bulliten.API/Models/Post.cs
+++ Bulliten.API/Models/Post.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text.Json.Serialization;
 
