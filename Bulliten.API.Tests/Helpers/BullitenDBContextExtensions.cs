08e552360e7d3461f064b30c7a4b4a44b5fc0c5e
David Wright <dontbothertheking@gmail.com>
2020-09-25 20:01:25 -0500

    Add helper extension methods

---
+++ Bulliten.API.Tests/Helpers/BullitenDBContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Bulliten.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Bulliten.API.Tests.Helpers
+{
+    public static class BullitenDBContextExtensions
+    {
+        public static void Setup(this BullitenDBContext context, Action<BullitenDBContext> action)
+        {
+            action.Invoke(context);
+            context.SaveChanges();
+        }
+
+        public static IEnumerable<UserAccount> GenerateUserAccounts(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                string username = $"User{i}";
+                yield return new UserAccount { Username = username, Password = "test" };
+            }
+        }
+    }
+}
