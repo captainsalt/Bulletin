f24340800c491c6e3e930d09f59e8dcf55fc110a
David Wright <dontbothertheking@gmail.com>
2020-09-26 15:36:44 -0500

    Create PostService

---
+++ Bulliten.API/Services/PostService.cs
@@ -0,0 +1,23 @@
+﻿using Bulliten.API.Middleware;
+using Bulliten.API.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bulliten.API.Services
+{
+    public class PostService
+    {
+        private readonly HttpContextAccessor _httpContextAccessor;
+
+        public PostService(HttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private UserAccount GetContextUser() =>
+            (UserAccount)_httpContextAccessor.HttpContext.Items[JwtMiddleware.CONTEXT_USER];
+    }
+}
