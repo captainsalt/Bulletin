c3e86aede07d65ca5234a14bd99e43c86507ff56
David Wright <dontbothertheking@gmail.com>
2020-09-30 13:15:03 -0500

    Don't map IDs for UserLike and UserRepost

--- Bulliten.API/Models/UserLike.cs
+++ Bulliten.API/Models/UserLike.cs
@@ -1,7 +1,12 @@
-﻿namespace Bulliten.API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bulliten.API.Models
 {
     public class UserLike : Entity
     {
+        [NotMapped]
+        public new int ID { get; set; }
+
         public int UserId { get; set; }
         public UserAccount User { get; set; }
 
