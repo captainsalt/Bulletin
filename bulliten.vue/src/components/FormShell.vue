04e76b12ddeee5e8f28df995df664b69d1d49263
David Wright <dontbothertheking@gmail.com>
2020-10-02 06:49:59 -0500

    Create FormShell component

---
+++ bulliten.vue/src/components/FormShell.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    class="card"
+    outlined
+    min-width="290px"
+    max-width="780px"
+  >
+    <form>
+      <slot/>
+    </form>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
