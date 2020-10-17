3e1fc0268a2a9f8b98bc6f034b513d86a2f2a1f5
David Wright <dontbothertheking@gmail.com>
2020-10-17 17:33:55 -0500

    Add button to create posts

--- src/bulliten.vue/src/views/Dashboard.vue
+++ src/bulliten.vue/src/views/Dashboard.vue
@@ -1,6 +1,14 @@
 <template>
   <SideNavLayout>
     <BullitenBoard :posts="posts"/>
+
+    <v-btn
+      v-resize="setButtonPos"
+      class="create"
+      color="secondary"
+    >
+      Create Post
+    </v-btn>
   </SideNavLayout>
 </template>
 
@@ -24,7 +32,23 @@ export default Vue.extend({
   },
   async beforeMount() {
     this.posts = await api.getPersonalFeed();
+  },
+  methods: {
+    setButtonPos() {
+      const createButton = this.$el.querySelector(".create") as HTMLElement;
+      const buttonHeight = createButton.offsetHeight;
+
+      createButton.style.top = `calc(100vh - ${buttonHeight}px - 12px)`;
+      return buttonHeight;
+    }
   }
 });
 </script>
 
+<style scoped>
+.create {
+  right: 12px;
+  position: fixed;
+  z-index: 1;
+}
+</style>
